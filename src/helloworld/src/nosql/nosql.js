ce618ed3948d632ff8d387faa326581afc730545
Eddy Ma <eddy.ma@smsmt.com>
2017-04-30 20:04:41 +1000

    Code Refactoring

--- src/helloworld/src/nosql/nosql.js
+++ src/helloworld/src/nosql/nosql.js
@@ -6,13 +6,16 @@ export class Nosql {
       { description: "Use Firebase as the backend database" },
       { description: "Use form and validation to add and update data" }
     ];
+    this.isLoading = false;
   }
 
   getData() {
+    this.isLoading = true;
     var self = this;
     var ref = firebase.database().ref("/Makes");
     ref.on('value', function (snapshot) {
       self.makes = (snapshot.val());
+      self.isLoading = false;
     }, function (errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
